fb0eabd415b71aab57602f94f217beff1e6b87d0
lukasthoms <luk@stho.ms>
2015-07-16 10:21:29 -0400

    finished lab

--- AnimatedValidator/ValidatorViewController.h
+++ AnimatedValidator/ValidatorViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ValidatorViewController : UIViewController
+@interface ValidatorViewController : UIViewController <UITextFieldDelegate>
 
 @end
--- AnimatedValidator/ValidatorViewController.m
+++ AnimatedValidator/ValidatorViewController.m
@@ -17,6 +17,7 @@
 @property (nonatomic, weak) IBOutlet UITextField *passwordTextField;
 @property (nonatomic, weak) IBOutlet UITextField *passwordConfirmTextField;
 @property (weak, nonatomic) IBOutlet UIButton *submitButton;
+@property (strong, nonatomic) NSArray *textFieldArray;
 
 @end
 
@@ -33,7 +34,48 @@
     self.phoneTextField.accessibilityLabel = PHONETEXTFIELD;
     self.passwordTextField.accessibilityLabel = PASSWORDTEXTFIELD;
     self.passwordConfirmTextField.accessibilityLabel = PASSWORDCONFIRMTEXTFIELD;
+    self.emailTextField.delegate = self;
+    self.emailConfirmTextField.delegate = self;
+    self.phoneTextField.delegate = self;
+    self.passwordTextField.delegate = self;
+    self.passwordConfirmTextField.delegate = self;
+    self.textFieldArray = @[self.emailTextField, self.emailConfirmTextField, self.phoneTextField, self.passwordTextField, self.passwordConfirmTextField];
     
+}
+
+- (BOOL)textFieldShouldEndEditing:(UITextField *)textField {
+    if ([textField.text isEqualToString:@""]) {
+        
+        [UIView animateKeyframesWithDuration:0.5 delay:0 options:0 animations:^{
+            [UIView addKeyframeWithRelativeStartTime:0 relativeDuration:0.5 animations:^{
+                textField.backgroundColor = [UIColor redColor];
+            }];
+            [UIView addKeyframeWithRelativeStartTime:0.5 relativeDuration:0.5 animations:^{
+                textField.backgroundColor = [UIColor whiteColor];
+            }];
+        } completion:^(BOOL finished) { }];
+        
+//        [UIView animateWithDuration:0.25 animations:^{
+//            textField.backgroundColor = [UIColor redColor];
+//
+//        }completion:^(BOOL finished) {
+//            [UIView animateWithDuration:0.25 animations:^{
+//                textField.backgroundColor = [UIColor whiteColor];
+//            }];
+//        }];
+        NSString *fixThis = [NSString stringWithFormat:@"Please fill this out: %@",textField.placeholder];
+        if (![textField.placeholder containsString:@"Please"]) {
+            textField.placeholder = fixThis;
+        }
+
+        return NO;
+    } else {
+        return YES;
+    }
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    return YES;
 }
 
 
